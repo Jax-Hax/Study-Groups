1c31ef057705fd7f24896edbe324e948c8aeff86
Jax-Hax <jaxhacks543@gmail.com>
2023-11-09 16:30:09 -0500

    adding styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,89 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import AttentionCatcher from './AttentionCatcher.svelte';
+	import Login from './Login.svelte';
+	import Signup from './Signup.svelte';
+	export let form;
+	let showSignup = false;
+	let showLogin = false;
+</script>
+
+<svelte:head>
+	<title>StockShare - Practice Trading Stocks with Friends</title>
+	<meta
+		name="description"
+		content="Learn about the stock market without risk. Invite your friends to a competition for free!"
+	/>
+</svelte:head>
+<header>
+	<img src="/favicon.png" alt="the logo for StockShare" style="width: min(9vw,5em)" />
+	<h1 id="nav-title"><a href="/" style="text-decoration: none">StockShare</a></h1>
+	<div>
+		<button id="signupButton" on:click={() => (showSignup = true)}>Get Started</button>
+		<button id="loginButton" on:click={() => (showLogin = true)}>Log In</button>
+	</div>
+</header>
+<body>
+	<section>
+		{#if showSignup}
+			<Signup {form} bind:showSignup bind:showLogin />
+		{/if}
+		{#if showLogin}
+			<Login {form} bind:showSignup bind:showLogin />
+		{/if}
+	</section>
+	<AttentionCatcher bind:showSignup/>
+</body>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		padding: 0.5em;
+		align-items: center;
+		background-color: #2c2c2c;
+	}
+	header a {
+		text-decoration: none;
+		color: white;
+		transition: all 0.3s ease 0s;
+	}
+	header a:hover {
+		color: var(--green);
+		text-decoration: underline;
+	}
+	@media screen and (max-width: 585px) {
+		header a{
+			display: none;
+		}
+	}
+	header button {
+		padding: 0.75em 2em;
+	}
+	#loginButton {
+		background-color: white;
+		color: var(--green);
+		cursor: pointer;
+		margin: -1px;
+		border: 1px solid var(--green);
+	}
+	#loginButton:hover {
+		background-color: #2bcf57;
+		color: white;
+		transform: none;
+		transition: none;
+	}
+	#signupButton {
+		background-color: var(--green);
+		color: white;
+		cursor: pointer;
+		border: none;
+		margin-right: 0.51em;
+	}
+	#signupButton:hover {
+		background-color: var(--dark-green);
+		color: white;
+	}
+	body {
+		background-color: #232323;
+	}
+</style>
