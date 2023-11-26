81e9905f647a45bf8c3c006f262f48d5ae59c741
Jax-Hax <jaxhacks543@gmail.com>
2023-11-26 14:16:15 -0800

    fixed css for class mod list

--- src/routes/admin/ClassMod.svelte
+++ src/routes/admin/ClassMod.svelte
@@ -4,26 +4,32 @@
 </script>
 
 <div id="grid">
-		{#each data.course_data as course}
-			<div>
-				<h1>{course.course_name}</h1>
-				{#each course.assignment_list as assignment}
-                <p>{assignment.text}</p>
-                <p>Due: {new Date(assignment.due_date).toLocaleString()}</p>
-                {/each}
-			</div>
-		{/each}
+	{#each data.course_data as course}
+		<div class="class">
+			<h1>{course.course_name}</h1>
+			{#each course.assignment_list as assignment}
+				<div class="assignment">
+					<div style="color: inherit; text-align: left">
+						<p>{assignment.text}</p>
+						<p>Due: {new Date(assignment.due_date).toLocaleString()}</p>
+					</div>
+						<button class="bouncyButton" style="--pop: var(--red); padding: 0.5em 1em; margin: 0.5em; margin-left: auto; margin-right: 0">Remove Post</button>
+						<button class="bouncyButton" style="--pop: var(--dark-red); padding: 0.5em 1em; margin: 0.5em;">Ban User</button>
+				</div>
+			{/each}
+		</div>
+	{/each}
 </div>
 
 <style>
 	#grid {
 		display: flex;
 		gap: 1rem;
-        flex-direction: column;
+		flex-direction: column;
 		align-items: center;
 		padding: 1em;
 	}
-	#grid div {
+	.class {
 		background-color: var(--background-4);
 		padding: 2em;
 		color: var(--text-color);
@@ -31,28 +37,17 @@
 		font-size: 1.25em;
 		border-radius: 16px;
 		text-align: center;
-		cursor: pointer;
-	}
-	#gpa {
-		background-color: var(--background-4);
-		padding: 2em;
-		color: var(--text-color);
-		border-radius: 16px;
-		font-size: 1.25em;
-		margin: 1rem 1rem 0rem 1rem;
-		border-radius: 16px;
-		text-align: center;
-		cursor: pointer;
-	}
-	#grid div:hover {
-		background-color: var(--background-6);
 	}
 	h1,
 	p {
 		color: inherit;
 	}
-	.textshadow {
-		color: transparent;
-		text-shadow: 0 0 5px var(--text-blur);
+	.assignment {
+		color: var(--text-color);
+		background-color: var(--background-6);
+		padding: 1em;
+		border-radius: 1em;
+		margin: 1em;
+		display: flex;
 	}
 </style>
