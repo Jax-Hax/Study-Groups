28619285b236298c56779c7397aecb80e928191a
Jax-Hax <jaxhacks543@gmail.com>
2023-12-03 10:57:37 -0500

    gotten deletions working on client side

--- src/routes/dashboard/ToDo.svelte
+++ src/routes/dashboard/ToDo.svelte
@@ -62,23 +62,24 @@
 		on:click={() => (showAddTask = true)}
 		style="padding: 0.5em 4em; font-size: 30px">Add Task</button
 	>
-	{#each assignment_array as todo}
+	{#each assignment_array as todo,i}
 		{@const course = data.course_data.filter((value) => value.course_id === todo.course_id)[0]}
 		{@const due_in = new Date(todo.due_date) - new Date()}
 		{#if todo.link !== null && todo.link !== ''}
 			<a href={todo.link} target="”_blank”">
 				<div class="hover">
 					<section>
-						{#if checked_assignment === todo.assignment_id}<button
+						{#if checked_assignment === todo.assignment_id}<form method="POST" action="?/deleteTodo"><button
+								on:click={(e) => {
+									checked_assignment = "";
+									assignment_array.splice(i,1)
+									e.preventDefault();
+								}}>Delete</button
+							></form>{:else}<button
 								on:click={(e) => {
 									checked_assignment = todo.assignment_id;
 									e.preventDefault();
 								}}>Delete</button
-							>{:else}<button
-								on:click={(e) => {
-									checked_assignment = todo.assignment_id;
-									e.preventDefault();
-								}}>not</button
 							>{/if}
 					</section>
 					<section style="flex-grow: 2">
